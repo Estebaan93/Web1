5deaffa7361b11edd70de574a37e23859ca45152
Estebaan93 <94663767+Estebaan93@users.noreply.github.com>
2023-10-29 15:36:49 -0300

    Practica 7JS

---
+++ Practicas/P7/js.js
@@ -0,0 +1,13 @@
+const regPeliculas=/^[a-zA-ZÀ-ÿ\s]{1,30}$/;  //Hasta 30 caracteres
+const regPuntuacion=/^\d{1}$/; 
+
+
+function validar(){
+
+    let pelicula=document.getElementById('pelicula');
+    let puntuacion=document.getElementById('puntuacion');
+    
+
+
+
+}
